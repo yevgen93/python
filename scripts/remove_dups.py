bbbf0796c9eeb25d948a7c0d7639d83066bed397
yevgeniy.zhitomirskiy <yevgeniy.zhitomirskiy@wework.com>
2019-09-27 14:26:18 -0400

    added remove dups in place

--- scripts/remove_dups.py
+++ scripts/remove_dups.py
@@ -1,8 +1,22 @@
-def removeDup(array):
-    final = []
-    for l in array:
-        if l not in final:
-            final.append(l)
-    return final
+# def removeDup(array):
+#     final = []
+#     for l in array:
+#         if l not in final:
+#             final.append(l)
+#     return final
 
-print(removeDup([1,1,2,2,3,3,4,4,5,5]))
+# print(removeDup([1,1,2,2,3,3,4,4,5,5]))
+
+# assume the input array is sorted
+# return the de-duped array in place
+
+def removeDup(array):
+    i = 0
+    while i<len(array)-1:
+        if array[i]==array[i+1]:
+            array.pop(i)
+        else:
+            i+=1
+    return array
+
+print(removeDup([1,1,2,2,3,3,4,4,5,5,6]))
