2bd74f5ffa970ae6c9381d1d6b33f0256cf72e0b
Yevgeniy Zhitomirskiy <yevgen93z@gmail.com>
2020-07-09 09:40:33 -0400

    Update reverse.py

--- scripts/reverse.py
+++ scripts/reverse.py
@@ -7,3 +7,14 @@ def reverse(str):
     return ''.join(final)
 
 print(reverse('cat'))
+
+def reverseString(self, s: List[str]) -> None:
+    left = 0
+    right = len(s)-1
+    while left < right:
+        temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+        left += 1
+        right -= 1
+    return s
