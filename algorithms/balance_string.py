b4ed2cb287f62d7c49fdc2a29acd3179d74e436b
Yevgeniy Zhitomirskiy <yevgen93z@gmail.com>
2020-07-30 19:52:56 -0400

    Create balance_string.py

---
+++ algorithms/balance_string.py
@@ -0,0 +1,14 @@
+# Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+# Given a balanced string s split it in the maximum amount of balanced strings.
+# Return the maximum amount of splitted balanced strings.
+
+def balancedStringSplit(self, s: str) -> int:
+    counter, final = 0,0
+    for char in s:
+        if char == "L":
+            counter += 1
+        else:
+            counter -= 1
+        if counter == 0:
+            final += 1
+    return final
