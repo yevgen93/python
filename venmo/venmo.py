f71040bf9ab9fcaa13248c66f32aaf7a66b63b92
Yevgeniy Zhitomirskiy <yevgen93z@gmail.com>
2022-05-26 11:12:03 -0400

    Create venmo.py

---
+++ venmo/venmo.py
@@ -0,0 +1,24 @@
+from venmo_api import Client, PaymentPrivacy
+
+class Venmo:
+    def __init__(self, access_token):
+        self.client = Client(access_token=access_token)
+
+    def get_user_id_by_username(self, username):
+        user = self.client.user.get_user_by_username(username=username)
+        if (user):
+            return user.id
+        else:
+            print("ERROR: user did not comeback. Check username.")
+            return None
+
+    def request_money(self, id, amount, description, callback = None):
+        # Returns a boolean: true if successfully requested
+        return self.client.payment.request_money(amount, description, id, PaymentPrivacy.PUBLIC, None, callback)
+
+
+access_token = "123456789"
+venmo = Venmo(access_token)
+john = venmo.get_user_id_by_username("John-Smith")
+#venmo.request_money(userId, 0.01, "test from venmo api")
+print(john)
