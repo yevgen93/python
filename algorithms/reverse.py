1fc4685ea3424ebc8437f634eddb460c7a3e0999
Yevgeniy Zhitomirskiy <yevgen93z@gmail.com>
2023-06-08 18:24:16 -0400

    Update reverse.py

--- algorithms/reverse.py
+++ algorithms/reverse.py
@@ -1,25 +1,3 @@
-def reverse(str):
-    final = []
-    n = (len(str)-1)
-    while n > -1:
-        final.append(str[n])
-        n -= 1
-    return ''.join(final)
-
-print(reverse('cat'))
-
-def reverseString(self, s: List[str]) -> None:
-    left = 0
-    right = len(s)-1
-    while left < right:
-        temp = s[left]
-        s[left] = s[right]
-        s[right] = temp
-        left += 1
-        right -= 1
-    return s
-
-
 # Write a function that reverses a string. The input string is given as an array of characters char[].
 # Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 # You may assume all the characters consist of printable ascii characters.
@@ -27,14 +5,24 @@ def reverseString(self, s: List[str]) -> None:
 # Input: ["h","e","l","l","o"]
 # Output: ["o","l","l","e","h"]
 
-class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        left = 0
-        right = len(s)-1
-        while left < right:
-            temp = s[left]
-            s[left] = s[right]
-            s[right] = temp
-            left += 1
-            right -= 1
-        return s
+def reverse(s):
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return s
+
+print(reverse(["h","e","l","l","o"]))
+
+# Allocate extra space
+# def reverse(str):
+#     final = []
+#     n = (len(str)-1)
+#     while n > -1:
+#         final.append(str[n])
+#         n -= 1
+#     return ''.join(final)
+
+# print(reverse('cat'))
