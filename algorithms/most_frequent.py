8eff4b4fa82b4374ffb48fda5e98a5904bdbc706
Yevgeniy Zhitomirskiy <yevgen93z@gmail.com>
2023-06-08 18:44:59 -0400

    Update most_frequent.py

--- algorithms/most_frequent.py
+++ algorithms/most_frequent.py
@@ -1,23 +1,23 @@
-# Given an array, what is the most frequently occuring element?
+# Given an array, find the most frequently occuring num.
 # array = [1,3,3,3,2,1,1,1]
-# most_frequent(array) should return 1
 
+def mostFrequent(arr):
+    map = {}
+    max_count = 0
+    most_frequent_num = None
 
-def most_frequent(list):
-  d = {}
-  max_count = 0
-  max_item = 0
-  
-  for item in list:
-    if item not in d:
-      # set the count of this item
-      d[item] = 1
-    else:
-      d[item] += 1
-    
-    if d[item] > max_count:
-      max_count = d[item]
-      max_item = item
-   
-  return max_item
-  
+    # Count occurence of each num
+    for num in arr:
+        if num in map.keys():
+            map[num] += 1
+        else:
+            map[num] = 1
+    # Check current occurence against max_count
+        if map[num] > max_count:
+            max_count = map[num]
+            most_frequent_num = num
+    return most_frequent_num
+
+print(mostFrequent([1,3,3,3,2,1,1,1])) # 1
+print(mostFrequent([1,3,3,3,2,1,1])) # 3
+print(mostFrequent([1,2,2,3])) # 2
