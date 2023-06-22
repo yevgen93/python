8fa1e044ec2f1bcaba23e2862a7c912d7c0a0c6b
Yevgeniy Zhitomirskiy <yevgen93z@gmail.com>
2023-06-22 10:06:59 -0400

    Update anagram.py

--- algorithms/anagram.py
+++ algorithms/anagram.py
@@ -1,47 +1,33 @@
 # Given an input of two strings, write a method to determine if the two strings are anagrams 
 # (composed of the same set of letters) of each other. Letter uppercase/lowercase state is ignored.
-# Sample inputs:
-# isAnagramMatch("melon", "lemon") => true
-# isAnagramMatch("ocean", "canoe") => true
-# isAnagramMatch("battle", "tablet") => true
-# isAnagramMatch("reserve", "reverse") => true
-# isAnagramMatch("bob", "rob") => false
-# isAnagramMatch("tape", "tap") => false
-
 # Without using return sorted(str1) == sorted(str2)
 
 def isAnagramMatch(str1, str2):
-    # Edge case if the strings are diff lengths
+    # Edge Case / strings are different lengths
     if len(str1) != len(str2):
         return False
-
-    # Build a hash map for each string with key/value pairs consisting of the letter and it's frequency in the string
+    
+    # Build hashmap per word of letter : count_of_letter
     map1, map2 = {}, {}
 
-    # Hashmap for first string
-    for letter in range(0,len(str1)):
-        l = str1[letter]
-        if l in map1.keys():
-            map1[l] += 1
+    for letter in str1:
+        if letter not in map1.keys():
+            map1[letter] = 1
         else:
-            map1[l] = 1
-    
-    # Hashmap for second string
-    for letter in range(0,len(str2)):
-        l = str2[letter]
-        if l in map2.keys():
-            map2[l] += 1
+            map1[letter] += 1
+
+    for letter in str2:
+        if letter not in map2.keys():
+            map2[letter] = 1
         else:
-            map2[l] = 1
-    
+            map2[letter] += 1
+
     # The two strings are anagrams if the corresponding maps are identical
     return map1 == map2
-    
 
-
-print(isAnagramMatch("melon","lemon"))
-print(isAnagramMatch("ocean","canoe"))
-print(isAnagramMatch("battle","tablet"))
-print(isAnagramMatch("reserve","reverse"))
-print(isAnagramMatch("bob","rob"))
-print(isAnagramMatch("tape","tap"))
+print(isAnagramMatch("melon", "lemon")) # True
+print(isAnagramMatch("ocean", "canoe")) # True
+print(isAnagramMatch("battle", "tablet")) # True
+print(isAnagramMatch("reserve", "reverse")) # True
+print(isAnagramMatch("bob", "rob")) # False
+print(isAnagramMatch("tape", "tap")) # False
