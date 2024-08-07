be2cae1a8687700ce7bb77f96eee6b9e06101fc4
Yevgeniy Zhitomirskiy <yevgen93z@gmail.com>
2024-08-07 10:12:39 -0400

    Create reverse_linked_list.py

---
+++ algorithms/reverse_linked_list.py
@@ -0,0 +1,27 @@
+# Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+# Example 1:
+# Input: head = [0,1,2,3]
+# Output: [3,2,1,0]
+
+# Example 2:
+# Input: head = []
+# Output: []
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def reverseList(head: ListNode):
+    prev = None
+    curr = head
+    
+    while curr:
+        next_node = curr.next # Store next node
+        curr.next = prev      # Reverse the link
+        prev = curr           # Move prev forward
+        curr = next_node      # Move current forward
+    return prev  
