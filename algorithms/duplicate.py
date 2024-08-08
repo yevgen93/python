cce68a0ababd432144623ab87e741c0199bbb509
Yevgeniy Zhitomirskiy <yevgen93z@gmail.com>
2024-08-08 12:06:43 -0400

    Update duplicate.py

--- algorithms/duplicate.py
+++ algorithms/duplicate.py
@@ -14,3 +14,18 @@ def hasDuplicate(nums):
             return True
         seen.add(num)
     return False
+
+
+def repeating_elements(nums):
+    seen = set()
+    final = []
+    for num in nums:
+        if num in seen:
+            final.append(num)
+        seen.add(num)
+    return final
+            
+
+print(repeating_elements([9, 8, 7, 8, 7, 6, 5]))  # expected output : [8, 7]
+print(repeating_elements([-1, 2, 3, -1, 2, 3]))   # expected output : [-1, 2, 3]
+print(repeating_elements([1, 2, 3, 4, 5]))        # expected output : []
